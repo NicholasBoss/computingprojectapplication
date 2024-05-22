4a9c2ebb18900daab44b74fdcbfeb19472d8d8b9
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-05-22 09:21:02 -0600

    Update insert_sqlite.py
    Made this file more consistent for implementation

--- insert_sqlite.py
+++ insert_sqlite.py
@@ -1,26 +1,26 @@
 class insert:
-    def __init__(self, cursor, conn):
-        self.cursor = cursor
-        self.conn = conn
+    def __init__(self, studentcursor, projectdb):
+        self.studentcursor = studentcursor
+        self.projectdb = projectdb
 
     def insert_data(self, project_name, project_description, username, current_date):
-        self.cursor.execute("""INSERT INTO project 
+        self.studentcursor.execute("""INSERT INTO project 
                             (project_name, project_description) 
                             VALUES 
                             (?, ?)""", 
                             (project_name, project_description))
-        self.conn.commit()
-        project_id = self.cursor.lastrowid
+        self.projectdb.commit()
+        project_id = self.studentcursor.lastrowid
         # print(f"PROJECT ID: {project_id}")
-        user_id = self.cursor.execute("""SELECT user_id 
-                                      FROM user 
-                                      WHERE username = ?""", (username,))
-        user_id = self.cursor.fetchone()
+        user_id = self.studentcursor.execute("""SELECT user_id 
+                                             FROM user 
+                                             WHERE username = ?""", (username,))
+        user_id = self.studentcursor.fetchone()
         user_id = user_id[0]
         # print(f"USER ID: {user_id}")
-        self.cursor.execute("""INSERT INTO user_project 
+        self.studentcursor.execute("""INSERT INTO user_project 
                             (user_id, project_id, date_added, last_updated_by, last_update_date) 
                             VALUES 
                             (?, ?, ?, ?, ?)""", 
                             (user_id, project_id, current_date, user_id, current_date))
-        self.conn.commit()
+        self.projectdb.commit()
